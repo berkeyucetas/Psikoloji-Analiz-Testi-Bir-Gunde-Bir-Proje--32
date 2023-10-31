392348c05bcd6898d18d58663b22a0ba7640fb75
berkeyucetas <berkeeyucetass@gmail.com>
2023-10-31 13:03:26 +0300

    Psikoloji Analiz Testi Bir Günde Bir Proje #32
    Bugün yaptığım projede internette bulunan onedio psikoloji testininin masaüstü halini yaptım.

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Psikoloji_Analiz_Testi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textSoru = new System.Windows.Forms.TextBox();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.btnE = new System.Windows.Forms.Button();
+            this.btnF = new System.Windows.Forms.Button();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.btnİleri = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textSoru
+            // 
+            this.textSoru.BackColor = System.Drawing.Color.Teal;
+            this.textSoru.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textSoru.ForeColor = System.Drawing.Color.White;
+            this.textSoru.Location = new System.Drawing.Point(12, 12);
+            this.textSoru.Multiline = true;
+            this.textSoru.Name = "textSoru";
+            this.textSoru.Size = new System.Drawing.Size(629, 256);
+            this.textSoru.TabIndex = 0;
+            // 
+            // btnA
+            // 
+            this.btnA.BackColor = System.Drawing.Color.Teal;
+            this.btnA.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnA.ForeColor = System.Drawing.Color.White;
+            this.btnA.Location = new System.Drawing.Point(12, 274);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(629, 39);
+            this.btnA.TabIndex = 1;
+            this.btnA.UseVisualStyleBackColor = false;
+            this.btnA.Click += new System.EventHandler(this.btnA_Click);
+            // 
+            // btnB
+            // 
+            this.btnB.BackColor = System.Drawing.Color.Teal;
+            this.btnB.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnB.ForeColor = System.Drawing.Color.White;
+            this.btnB.Location = new System.Drawing.Point(12, 319);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(629, 39);
+            this.btnB.TabIndex = 2;
+            this.btnB.UseVisualStyleBackColor = false;
+            this.btnB.Click += new System.EventHandler(this.btnB_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.BackColor = System.Drawing.Color.Teal;
+            this.btnC.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnC.ForeColor = System.Drawing.Color.White;
+            this.btnC.Location = new System.Drawing.Point(12, 364);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(629, 39);
+            this.btnC.TabIndex = 3;
+            this.btnC.UseVisualStyleBackColor = false;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btnD
+            // 
+            this.btnD.BackColor = System.Drawing.Color.Teal;
+            this.btnD.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnD.ForeColor = System.Drawing.Color.White;
+            this.btnD.Location = new System.Drawing.Point(12, 409);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(629, 39);
+            this.btnD.TabIndex = 4;
+            this.btnD.UseVisualStyleBackColor = false;
+            this.btnD.Click += new System.EventHandler(this.btnD_Click);
+            // 
+            // btnE
+            // 
+            this.btnE.BackColor = System.Drawing.Color.Teal;
+            this.btnE.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnE.ForeColor = System.Drawing.Color.White;
+            this.btnE.Location = new System.Drawing.Point(12, 454);
+            this.btnE.Name = "btnE";
+            this.btnE.Size = new System.Drawing.Size(629, 39);
+            this.btnE.TabIndex = 5;
+            this.btnE.UseVisualStyleBackColor = false;
+            this.btnE.Click += new System.EventHandler(this.btnE_Click);
+            // 
+            // btnF
+            // 
+            this.btnF.BackColor = System.Drawing.Color.Teal;
+            this.btnF.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnF.ForeColor = System.Drawing.Color.White;
+            this.btnF.Location = new System.Drawing.Point(12, 499);
+            this.btnF.Name = "btnF";
+            this.btnF.Size = new System.Drawing.Size(629, 39);
+            this.btnF.TabIndex = 6;
+            this.btnF.UseVisualStyleBackColor = false;
+            this.btnF.Click += new System.EventHandler(this.btnF_Click);
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.BackColor = System.Drawing.Color.Teal;
+            this.btnBasla.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBasla.ForeColor = System.Drawing.Color.White;
+            this.btnBasla.Location = new System.Drawing.Point(895, 12);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(104, 47);
+            this.btnBasla.TabIndex = 7;
+            this.btnBasla.Text = "BAŞLA";
+            this.btnBasla.UseVisualStyleBackColor = false;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // btnİleri
+            // 
+            this.btnİleri.BackColor = System.Drawing.Color.Teal;
+            this.btnİleri.BackgroundImage = global::Psikoloji_Analiz_Testi.Properties.Resources.indir;
+            this.btnİleri.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnİleri.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnİleri.ForeColor = System.Drawing.Color.White;
+            this.btnİleri.Location = new System.Drawing.Point(647, 12);
+            this.btnİleri.Name = "btnİleri";
+            this.btnİleri.Size = new System.Drawing.Size(56, 55);
+            this.btnİleri.TabIndex = 8;
+            this.btnİleri.UseVisualStyleBackColor = false;
+            this.btnİleri.Click += new System.EventHandler(this.btnİleri_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1011, 609);
+            this.Controls.Add(this.btnİleri);
+            this.Controls.Add(this.btnBasla);
+            this.Controls.Add(this.btnF);
+            this.Controls.Add(this.btnE);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.textSoru);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textSoru;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Button btnE;
+        private System.Windows.Forms.Button btnF;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Button btnİleri;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,405 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Psikoloji_Analiz_Testi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int soru;
+        int toplam = 0;
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            btnİleri.Visible = true;
+            btnBasla.Enabled = false;
+            btnBasla.Text = "BİTİR";
+            soru++;
+            if (soru == 1)
+            {
+                textSoru.Text = "1- Sevgilinizle ya da eşinizle mükemmel bir akşam size aşağıdakilerden hangisini çağrıştırıyor?";
+                btnA.Text = "KONSERE GİTMEK";
+                btnB.Text = "SİNEMAYA GİTMEK";
+                btnC.Text = "SAHİL KENARINDA GEZMEK";
+                btnD.Text = "ROMANTİK BİR AKŞAM YEMEĞİ";
+                btnE.Text = "ARABAYLA TURLAMAK";
+                btnF.Text = "LUNAPARKA GİTMEK";
+                btnA.Enabled = true;
+                btnB.Enabled = true;
+                btnC.Enabled = true;
+                btnD.Enabled = true;
+                btnE.Enabled = true;
+                btnF.Enabled = true;
+
+            }
+            if (soru == 11)
+            {
+                btnİleri.Visible= false;
+                btnA.Text = "Skorunuz: "+ toplam.ToString();
+                Font mevcutFont = btnA.Font;                
+                Font yeniFont = new Font(mevcutFont.FontFamily, 18, mevcutFont.Style);
+                btnA.Font = yeniFont;
+                btnB.Visible = false;
+                btnC.Visible = false;
+                btnD.Visible = false;
+                btnE.Visible = false;
+                btnF.Visible = false;
+                if (toplam >= 10 && toplam <= 20)
+                {
+                    textSoru.Text = "Siz vahşi ve çılgın birisisiniz eğleneyim derken uçuyorsunuz.";
+                }
+                else if (toplam >= 21 && toplam <= 30)
+                {
+                    textSoru.Text = "Siz eğlenceli, arkadaş canlısı ve popülersiniz.";
+                }  
+                else if (toplam >= 31 && toplam <= 40)
+                {
+                    textSoru.Text = "Öyle tatlısınızkii sizi sevmemek mümkün değil insanların kaybetmek istemiyeceği türden bir insansınız.";
+                }
+                else if (toplam >= 41 && toplam <= 50)
+                {
+                    textSoru.Text = "Siz iflah olmaz bir romantiksiniz şarap, kırmızı güller bunlar sisin yaşam tarzınız.";
+                }
+                else if (toplam >= 51 && toplam <= 60)
+                {
+                    textSoru.Text = "Belirgin özelliğiniz hızlı ve mantıklı karar verebilmeniz ve her olay için kafanızın içinde plan olmasıdır.";
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnİleri.Visible=false;
+        }
+
+        private void btnİleri_Click(object sender, EventArgs e)
+        {
+            soru++;
+            if (soru == 2)
+            {
+                textSoru.Text = "2- En sevdiğiniz müzik türü nedir?";
+                btnA.Text = "POP";
+                btnB.Text = "ROCK";
+                btnC.Text = "RAP";
+                btnD.Text = "ARABESK";
+                btnE.Text = "KLASİK";
+                btnF.Text = "YABANCI";
+                btnA.BackColor = Color.Teal;
+                btnB.BackColor = Color.Teal;
+                btnC.BackColor = Color.Teal;
+                btnD.BackColor = Color.Teal;
+                btnE.BackColor = Color.Teal;
+                btnF.BackColor = Color.Teal;
+                btnA.ForeColor = Color.White;
+                btnB.ForeColor = Color.White;
+                btnC.ForeColor = Color.White;
+                btnD.ForeColor = Color.White;
+                btnE.ForeColor = Color.White;
+                btnF.ForeColor = Color.White;
+                btnA.Enabled = true;
+                btnB.Enabled = true;
+                btnC.Enabled = true;
+                btnD.Enabled = true;
+                btnE.Enabled = true;
+                btnF.Enabled = true;
+            }
+            if (soru == 3)
+            {
+                textSoru.Text = "3- En çok hangi film türünü seversiniz?";
+                btnA.Text = "KORKU";
+                btnB.Text = "ROMANTİK";
+                btnC.Text = "MACERA";
+                btnD.Text = "AKSİYON";
+                btnE.Text = "KOMEDİ";
+                btnF.Text = "BELGESEL"; 
+                btnA.BackColor = Color.Teal;
+                btnB.BackColor = Color.Teal;
+                btnC.BackColor = Color.Teal;
+                btnD.BackColor = Color.Teal;
+                btnE.BackColor = Color.Teal;
+                btnF.BackColor = Color.Teal;
+                btnA.ForeColor = Color.White;
+                btnB.ForeColor = Color.White;
+                btnC.ForeColor = Color.White;
+                btnD.ForeColor = Color.White;
+                btnE.ForeColor = Color.White;
+                btnF.ForeColor = Color.White;
+                btnA.Enabled = true;
+                btnB.Enabled = true;
+                btnC.Enabled = true;
+                btnD.Enabled = true;
+                btnE.Enabled = true;
+                btnF.Enabled = true;
+            }
+            if (soru == 4)
+            {
+                textSoru.Text = "4- Aşağıdaki mesleklerden birisini seçecek olsanız hangisini seçerdiniz?";
+                btnA.Text = "PİLOT";
+                btnB.Text = "ÖĞRETMEN";
+                btnC.Text = "DOKTOR";
+                btnD.Text = "ASKER";
+                btnE.Text = "KOMEDYEN";
+                btnF.Text = "FUTBOLCU";
+                btnA.BackColor = Color.Teal;
+                btnB.BackColor = Color.Teal;
+                btnC.BackColor = Color.Teal;
+                btnD.BackColor = Color.Teal;
+                btnE.BackColor = Color.Teal;
+                btnF.BackColor = Color.Teal;
+                btnA.ForeColor = Color.White;
+                btnB.ForeColor = Color.White;
+                btnC.ForeColor = Color.White;
+                btnD.ForeColor = Color.White;
+                btnE.ForeColor = Color.White;
+                btnF.ForeColor = Color.White;
+                btnA.Enabled = true;
+                btnB.Enabled = true;
+                btnC.Enabled = true;
+                btnD.Enabled = true;
+                btnE.Enabled = true;
+                btnF.Enabled = true;
+            }
+            if (soru == 5)
+            {
+                textSoru.Text = "5- Bir saat boşluğunuz var bunu aşağıdakilerden hangisi ile değerlendirirsiniz?";
+                btnA.Text = "KİTAP OKUYARAK";
+                btnB.Text = "UYUYARAK";
+                btnC.Text = "OYUN OYNAYARAK";
+                btnD.Text = "YEMEK YİYEREK";
+                btnE.Text = "TELEVİZYON İZLEYEREK";
+                btnF.Text = "GEZEREK";
+                btnA.BackColor = Color.Teal;
+                btnB.BackColor = Color.Teal;
+                btnC.BackColor = Color.Teal;
+                btnD.BackColor = Color.Teal;
+                btnE.BackColor = Color.Teal;
+                btnF.BackColor = Color.Teal;
+                btnA.ForeColor = Color.White;
+                btnB.ForeColor = Color.White;
+                btnC.ForeColor = Color.White;
+                btnD.ForeColor = Color.White;
+                btnE.ForeColor = Color.White;
+                btnF.ForeColor = Color.White;
+                btnA.Enabled = true;
+                btnB.Enabled = true;
+                btnC.Enabled = true;
+                btnD.Enabled = true;
+                btnE.Enabled = true;
+                btnF.Enabled = true;
+            }
+            if (soru == 6)
+            {
+                textSoru.Text = "6- Aşağıdaki renklerden birisini seçiniz?";                
+                btnA.Text = "";
+                btnB.Text = "";
+                btnC.Text = "";
+                btnD.Text = "";
+                btnE.Text = "";
+                btnF.Text = "";
+                btnA.BackColor = Color.Yellow;
+                btnB.BackColor = Color.Green;
+                btnC.BackColor = Color.Pink;
+                btnD.BackColor = Color.Red;
+                btnE.BackColor = Color.Blue;
+                btnF.BackColor = Color.Brown;
+                btnA.ForeColor = Color.White;
+                btnB.ForeColor = Color.White;
+                btnC.ForeColor = Color.White;
+                btnD.ForeColor = Color.White;
+                btnE.ForeColor = Color.White;
+                btnF.ForeColor = Color.White;
+                btnA.Enabled = true;
+                btnB.Enabled = true;
+                btnC.Enabled = true;
+                btnD.Enabled = true;
+                btnE.Enabled = true;
+                btnF.Enabled = true;
+            }
+            if (soru == 7)
+            {
+                textSoru.Text = "7- Şuan önünüzde hangi yiyecek olsa dayanamayıp yersiniz?";
+                btnA.Text = "ÇİKOLATA";
+                btnB.Text = "HAMBURGER";
+                btnC.Text = "PİZZA";
+                btnD.Text = "MAKARNA";
+                btnE.Text = "PASTA";
+                btnF.Text = "SUŞİ";
+                btnA.BackColor = Color.Teal;
+                btnB.BackColor = Color.Teal;
+                btnC.BackColor = Color.Teal;
+                btnD.BackColor = Color.Teal;
+                btnE.BackColor = Color.Teal;
+                btnF.BackColor = Color.Teal;
+                btnA.ForeColor = Color.White;
+                btnB.ForeColor = Color.White;
+                btnC.ForeColor = Color.White;
+                btnD.ForeColor = Color.White;
+                btnE.ForeColor = Color.White;
+                btnF.ForeColor = Color.White;
+                btnA.Enabled = true;
+                btnB.Enabled = true;
+                btnC.Enabled = true;
+                btnD.Enabled = true;
+                btnE.Enabled = true;
+                btnF.Enabled = true;
+            }
+            if (soru == 8)
+            {
+                textSoru.Text = "8- En sevdiğiniz tatil, özel gün ya da bayram hangisidir? ";
+                btnA.Text = "23 NİSAN ULUSAL EGEMENLİK VE ÇOCUK BAYRAMI";
+                btnB.Text = "HAFTA SONU TATİLİ";
+                btnC.Text = "RAMAZAN BAYRAMI";
+                btnD.Text = "KURBAN BAYRAMI";
+                btnE.Text = "YILBAŞI TATİLİ";
+                btnF.Text = "SEVGİLİLER GÜNÜ";
+                btnA.BackColor = Color.Teal;
+                btnB.BackColor = Color.Teal;
+                btnC.BackColor = Color.Teal;
+                btnD.BackColor = Color.Teal;
+                btnE.BackColor = Color.Teal;
+                btnF.BackColor = Color.Teal;
+                btnA.ForeColor = Color.White;
+                btnB.ForeColor = Color.White;
+                btnC.ForeColor = Color.White;
+                btnD.ForeColor = Color.White;
+                btnE.ForeColor = Color.White;
+                btnF.ForeColor = Color.White;
+                btnA.Enabled = true;
+                btnB.Enabled = true;
+                btnC.Enabled = true;
+                btnD.Enabled = true;
+                btnE.Enabled = true;
+                btnF.Enabled = true;
+            }
+            if (soru == 9)
+            {
+                textSoru.Text = "9- Şuan aşağıdakilerden hangisinde olmak isterdin?";
+                btnA.Text = "ALMANYA";
+                btnB.Text = "NEW YORK";
+                btnC.Text = "DUBAİ";
+                btnD.Text = "KIBRIS";
+                btnE.Text = "İTALYA";
+                btnF.Text = "JAPONYA";
+                btnA.BackColor = Color.Teal;
+                btnB.BackColor = Color.Teal;
+                btnC.BackColor = Color.Teal;
+                btnD.BackColor = Color.Teal;
+                btnE.BackColor = Color.Teal;
+                btnF.BackColor = Color.Teal;
+                btnA.ForeColor = Color.White;
+                btnB.ForeColor = Color.White;
+                btnC.ForeColor = Color.White;
+                btnD.ForeColor = Color.White;
+                btnE.ForeColor = Color.White;
+                btnF.ForeColor = Color.White;
+                btnA.Enabled = true;
+                btnB.Enabled = true;
+                btnC.Enabled = true;
+                btnD.Enabled = true;
+                btnE.Enabled = true;
+                btnF.Enabled = true;
+            }
+            if (soru == 10)
+            {
+                textSoru.Text = "10- Genel olarak nasıl insanlarla vakit geçirmekten hoşlanırsın?";
+                btnA.Text = "PARTİ CANAVARI BİRİYLE";
+                btnB.Text = "SAKİN BİRİYLE";
+                btnC.Text = "KOMİK BİRİYLE";
+                btnD.Text = "OYUNCU BİRİYLE";
+                btnE.Text = "GÜZEL/YAKIŞIKLI BİRİYLE";
+                btnF.Text = "AKILLI BİRİYLE";
+                btnA.BackColor = Color.Teal;
+                btnB.BackColor = Color.Teal;
+                btnC.BackColor = Color.Teal;
+                btnD.BackColor = Color.Teal;
+                btnE.BackColor = Color.Teal;
+                btnF.BackColor = Color.Teal;
+                btnA.ForeColor = Color.White;
+                btnB.ForeColor = Color.White;
+                btnC.ForeColor = Color.White;
+                btnD.ForeColor = Color.White;
+                btnE.ForeColor = Color.White;
+                btnF.ForeColor = Color.White;
+                btnİleri.Enabled = false;
+                btnİleri.Visible = false;
+                btnBasla.Enabled = true;
+                btnA.Enabled = true;
+                btnB.Enabled = true;
+                btnC.Enabled = true;
+                btnD.Enabled = true;
+                btnE.Enabled = true;
+                btnF.Enabled = true;
+
+            }            
+        }
+        private void btnA_Click(object sender, EventArgs e)
+        {
+            toplam = toplam + 4;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnE.Enabled = false;
+            btnF.Enabled = false;
+        }
+
+        private void btnB_Click(object sender, EventArgs e)
+        {
+            toplam = toplam + 1;
+            btnA.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnE.Enabled = false;
+            btnF.Enabled = false;
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            toplam = toplam + 3;
+            btnB.Enabled = false;
+            btnA.Enabled = false;
+            btnD.Enabled = false;
+            btnE.Enabled = false;
+            btnF.Enabled = false;
+        }
+
+        private void btnD_Click(object sender, EventArgs e)
+        {
+            toplam = toplam + 6;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnA.Enabled = false;
+            btnE.Enabled = false;
+            btnF.Enabled = false;
+        }
+
+        private void btnE_Click(object sender, EventArgs e)
+        {
+            toplam = toplam + 2;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnA.Enabled = false;
+            btnF.Enabled = false;
+        }
+
+        private void btnF_Click(object sender, EventArgs e)
+        {
+            toplam = toplam + 5;
+            btnB.Enabled = false;
+            btnC.Enabled = false;
+            btnD.Enabled = false;
+            btnE.Enabled = false;
+            btnA.Enabled = false;
+        }
+    }
+}
